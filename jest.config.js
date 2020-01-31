8f47434b4b8e27ec8268a7104ef65ceed4e56fcb
Pierre-M <meriaupierre.pro@gmail.com>
2020-01-31 17:44:23 +0100

    add more coverage fields

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
     preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
     collectCoverage: true,
-    collectCoverageFrom: [`<rootDir>/src/App.vue`, `<rootDir>/src/core/**/*.ts`, `<rootDir>/src/store/**/*.ts`],
+    collectCoverageFrom: [
+        `<rootDir>/src/App.vue`,
+        `<rootDir>/src/core/**/*.ts`,
+        `<rootDir>/src/store/**/*.ts`,
+        `<rootDir>/src/business/**/*.ts`
+    ],
     setupFiles: [`<rootDir>/tests/unit/index.ts`]
 };
