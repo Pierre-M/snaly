d380ac0a7290e12addbce88c4b86c23be5ae5e3c
Pierre-M <meriaupierre.pro@gmail.com>
2020-02-14 11:12:47 +0100

    Try to fix gh action again

--- tests/e2e/specs/CitySearch.spec.js
+++ tests/e2e/specs/CitySearch.spec.js
@@ -2,8 +2,7 @@
 
 describe("City Search Feature", () => {
     it("should be able to display temperature for searched city", () => {
-        console.log("baseUrl", Cypress.env("baseUrl"));
-        cy.visit("/");
+        cy.visit(Cypress.env("baseUrl"));
         cy.get("#openCitySearch").click();
         cy.get("#citySearchQueryInput").type("Lyon");
         cy.get("#citySearchResults li:first-child").click();
