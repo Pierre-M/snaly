7a4e28aea27670a89b7f12c4dfc3c8a0af937f36
Pierre-M <meriaupierre.pro@gmail.com>
2020-02-23 22:23:24 +0100

    Fix doubled actions in header

--- src/ui/core/fundamentals/SharingCta.vue
+++ src/ui/core/fundamentals/SharingCta.vue
@@ -1,21 +1,17 @@
 <template>
-    <icon-btn v-if="canShare" icon="share" @click="share" :label="$t('share.ctaLabel')" />
+    <icon-btn icon="share" @click="share" :label="$t('share.ctaLabel')" />
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import IconBtn from "./IconBtn.vue";
-import { AppState } from "@/store/store";
-import { Action, State } from "vuex-class";
+import { Action } from "vuex-class";
 import { StoreAction } from "@/store/actions";
 
 @Component({
     components: { IconBtn }
 })
 export default class SharingCta extends Vue {
-    @State((state: AppState) => state.uiModule.canShare)
-    canShare!: boolean;
-
     @Action(StoreAction.SHARE)
     share!: () => Promise<void>;
 }
